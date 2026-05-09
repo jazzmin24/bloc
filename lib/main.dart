a8b8a3fc049ec40f628ca72638eef7e4b44b7f08
jasmin <jasmin@goodeducator.com>
2026-05-09 13:20:54 +0530

    learnt equatable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+//------- WITH EQUATABLE -------
 import 'dart:developer';
-
+import 'package:equatable/equatable.dart'; //imported equatable package
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,43 +35,112 @@ class _EquatableTestingState extends State<EquatableTesting> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(onPressed: () {
-        Person person1 = const Person(name: "John", age: 30);
+        Person person1 = const Person(name: "messi", age: 30);
         const Person person2 = Person(name: "John", age: 30);
         Person person3 = Person(name: "John", age: 30);
 
-        log("message 1: ${person1 == person2}"); // false
-        log("message 2: ${person1 == person3}"); // true
-        log("message 3: ${person2 == person3}"); // true
+        log("message 1: ${person1 == person2}");
+        log("message 2: ${person1 == person3}");
+        log("message 3: ${person2 == person3}");
 
-        log("hash code 1: ${person1.hashCode}"); // different hash code
-        log("hash code 2: ${person2.hashCode}"); // different hash code
-        log("hash code 3: ${person3.hashCode}"); // different hash code
-
-        person1 == person2;
-        person1 == person3;
-        person2 == person3;
+        log("hash code 1: ${person1.hashCode}");
+        log("hash code 2: ${person2.hashCode}");
+        log("hash code 3: ${person3.hashCode}");
       }),
     );
   }
 }
 
-class Person {
+class Person extends Equatable {
   final String name;
   final int age;
   const Person({required this.name, required this.age});
 
-  bool operator ==(Object other) {
-    return identical(this, other) // same object in memory? shortcut
-        ||
-        other is Person // is it even a Person?
-            &&
-            runtimeType == other.runtimeType // exact same class?
-            &&
-            name == other.name // same name?
-            &&
-            age == other.age; // same age?
-  }
-
+  // Equatable package automatically generates the == operator and hashCode based on the list of properties provided in the props getter.
   @override
-  int get hashCode => Object.hash(name, age);
+  List<Object> get props => [name, age];
 }
+
+
+
+
+
+
+//------- WITHOUT EQUATABLE -------
+// import 'dart:developer';
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: EquatableTesting(),
+//     );
+//   }
+// }
+
+// class EquatableTesting extends StatefulWidget {
+//   const EquatableTesting({super.key});
+
+//   @override
+//   State<EquatableTesting> createState() => _EquatableTestingState();
+// }
+
+// class _EquatableTestingState extends State<EquatableTesting> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       floatingActionButton: FloatingActionButton(onPressed: () {
+//         Person person1 = const Person(name: "John", age: 30);
+//         const Person person2 = Person(name: "John", age: 30);
+//         Person person3 = Person(name: "John", age: 30);
+
+//         log("message 1: ${person1 == person2}"); // false
+//         log("message 2: ${person1 == person3}"); // true
+//         log("message 3: ${person2 == person3}"); // true
+
+//         log("hash code 1: ${person1.hashCode}"); // different hash code
+//         log("hash code 2: ${person2.hashCode}"); // different hash code
+//         log("hash code 3: ${person3.hashCode}"); // different hash code
+
+//         person1 == person2;
+//         person1 == person3;
+//         person2 == person3;
+//       }),
+//     );
+//   }
+// }
+
+// class Person {
+//   final String name;
+//   final int age;
+//   const Person({required this.name, required this.age});
+
+//   bool operator ==(Object other) {
+//     return identical(this, other) // same object in memory? shortcut
+//         ||
+//         other is Person // is it even a Person?
+//             &&
+//             runtimeType == other.runtimeType // exact same class?
+//             &&
+//             name == other.name // same name?
+//             &&
+//             age == other.age; // same age?
+//   }
+
+//   @override
+//   int get hashCode => Object.hash(name, age);
+// }
