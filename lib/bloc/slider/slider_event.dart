22e954d0c942d7639039bea30a90c7a7a19d6707
jasmin <jasmin@goodeducator.com>
2026-05-10 12:12:40 +0530

    fix

--- lib/bloc/slider/slider_event.dart
+++ lib/bloc/slider/slider_event.dart
@@ -9,8 +9,13 @@ sealed class SliderEvent extends Equatable {
 
 class SliderValueEvent extends SliderEvent {
   double slider;
-  SliderValueEvent({required this.slider});
+  SliderValueEvent({required this.slider});              //bloc has no way to know where the user dragged it — the value must travel inside the event
 
   @override
   List<Object> get props => [slider];
 }
+
+
+
+//COUNTER - Value calculated	inside bloc (+1/-1)	
+//SLIDER - Value calculated comes from UI
